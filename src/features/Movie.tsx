384af497b34bc6e485af0b844bf072b61e3d8220
tijanab <baractijana@gmail.com>
2022-02-13 15:33:54 +0100

    Set up Movie

--- src/features/Movie.tsx
+++ src/features/Movie.tsx
@@ -1,7 +1,22 @@
-const Movie = () => {
+import NoImage from 'src/res/no-image.png'
+
+const placeholder = NoImage
+
+const Movie = (movie: any) => {
+  const poster = (movie.Poster === "N/A" ? placeholder : movie.Poster)
   return (
-    <p>A movie displayed</p>
-  )
+    <div className='movie'>
+      <h2>{movie.Title}</h2>
+      <div>
+        <img
+          alt={`${movie.Title}`}
+					className='movie-image'
+          src={poster}
+        />
+      </div>
+      <p>Year of publication: {movie.Year}</p>
+    </div>
+  );
 }
 
 export default Movie
